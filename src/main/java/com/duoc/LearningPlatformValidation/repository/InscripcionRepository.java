24c1e883577b3e77fbaa160f6a96e9aa251c350d
Rafael Navarrete <ra.navarretem@duocuc.cl>
2026-05-03 13:43:31 -0400

    UsuarioRepository e InscripcionRepository terminados

--- src/main/java/com/duoc/LearningPlatformValidation/repository/InscripcionRepository.java
+++ src/main/java/com/duoc/LearningPlatformValidation/repository/InscripcionRepository.java
@@ -1,5 +1,20 @@
 package com.duoc.LearningPlatformValidation.repository;
 
-public class InscripcionRepository {
-    
+import com.duoc.LearningPlatformValidation.model.Inscripcion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+/**
+ * Repositorio JPA para la entidad Inscripcion.
+ * Proporciona métodos de acceso a datos e incluye una consulta
+ * personalizada para buscar inscripciones por ID de curso.
+ *
+ * @author Valeria Acevedo & Rafael Navarrete
+ */
+
+@Repository
+public interface InscripcionRepository extends JpaRepository<Inscripcion, Long> {
+    // Busca todas las inscripciones asociadas a un curso específico
+    List<Inscripcion> findByCursoId(Long cursoId);
 }
--- src/main/java/com/duoc/LearningPlatformValidation/repository/UsuarioRepository.java
+++ src/main/java/com/duoc/LearningPlatformValidation/repository/UsuarioRepository.java
@@ -1,5 +1,17 @@
 package com.duoc.LearningPlatformValidation.repository;
 
-public class UsuarioRepository {
-    
+import com.duoc.LearningPlatformValidation.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repositorio JPA para la entidad Usuario.
+ * Proporciona automáticamente los métodos de acceso a datos
+ * como guardar, buscar, listar y eliminar usuarios.
+ *
+ * @author Valeria Acevedo & Rafael Navarrete
+ */
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
